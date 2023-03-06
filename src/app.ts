0dca0fea9ffef2cb396086600f63299dbf7cd81e
RomanKosmyna <roma.kosmyna@gmail.com>
2023-03-07 00:53:30 +0200

    Added devDeps

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import * as mongoose from "mongoose";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = 5100;
+
+app.listen(PORT, () => {
+  mongoose.connect("mongodb://127.0.0.1:27017/test");
+  console.log("Server has been started!");
+});
